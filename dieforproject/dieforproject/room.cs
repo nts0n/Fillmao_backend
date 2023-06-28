a8966ced4b8d7015c8cd87db76943bbe8d359e17
nts0n <nguyentheson1206@gmail.com>
2023-06-28 09:52:11 +0700

    a bit change :3

--- dieforproject/dieforproject/room.cs
+++ dieforproject/dieforproject/room.cs
@@ -37,17 +37,21 @@ namespace dieforproject
         };
         IFirebaseClient xx;
 
-        void send(Socket client)
+        void send(Socket client, string msg)
         {
-            string msg = "ok dc r";
-            byte[] send_data = System.Text.Encoding.UTF8.GetBytes(msg);
+            string s = "fuckiiiii";
+            byte[] send_data = System.Text.Encoding.UTF8.GetBytes(s);
             ns = new NetworkStream(client);
+      
             ns.Write(send_data, 0, send_data.Length);
         }
 
-        void room_create()
+        void room_create(roomdetail room_Recv, Socket client)
         {
-            
+            string idphong = r.Next(100000, 999999).ToString();
+            room_Recv.RoomId = idphong;
+            var ssetter = xx.Set("room/" + idphong, room_Recv);
+            send(client, idphong);
         }
         void receive_and_broacast(object obj)
         {
@@ -64,39 +68,47 @@ namespace dieforproject
                         client.Receive(recvd);
                         msg += Encoding.UTF8.GetString(recvd);
                     }while (msg[msg.Length-1] != '\0' && msg[msg.Length-1] != '\n');
-                    Player x;
+                    
 
                     
                     if (msg != null && msg[0] != '\0')
                     {
                         Console.WriteLine(msg);
-                        string[] temp = msg.Split(", ");
-                        string option = temp[0];
-                        Console.WriteLine(temp[1]);
+                        string[] response_Recv = msg.Split(", ");
+                        string option = response_Recv[0];
+                        Console.WriteLine(response_Recv[1]);
                         if (option == "login")
                         {
-                            x = JsonConvert.DeserializeObject<Player>(temp[1]);
+                            Player player_Info;
+                            player_Info = JsonConvert.DeserializeObject<Player>(response_Recv[1]);
                             string idngchoi = r.Next(100000, 999999).ToString();
                             Player player = new Player(
                                 idngchoi,
-                                x.PlayerName
+                                player_Info.PlayerName
 
                             );
                             var ssetter = xx.Set("player/" + idngchoi, player);
+                            send(client, idngchoi);
                         }
                         else if(option == "create_room")
                         {
-                            room_create();
+                            roomdetail room_Recv;
+                            room_Recv = JsonConvert.DeserializeObject<roomdetail>(response_Recv[1]);
+                            room_create(room_Recv, client);
                         }
-                        foreach (Socket c in clients)
-                        {
-                            send(c);
-                        }
+                        
                     }
                     else if ((client.Poll(-1, SelectMode.SelectRead)) || (client.Available != 0))
                     {
                         Console.WriteLine("LOLOLOLOLOLO");
+                        clients.Remove(client);
                         
+                        client.Close();
+                        if (clients.Count == 0)
+                        {
+                            Console.WriteLine("bo m dong server");
+                            server_socket.Close();
+                        }
                         break;
 
                     }
@@ -117,9 +129,14 @@ namespace dieforproject
             }
             catch
             {
-                Console.WriteLine("1 thz out");
+                Console.WriteLine("1 thz ou   t");
                 clients.Remove(client);
                 client.Close();
+                if (clients.Count == 0)
+                {
+                    Console.WriteLine("bo m dong server");
+                    server_socket.Close();
+                }
             }
         }
         
